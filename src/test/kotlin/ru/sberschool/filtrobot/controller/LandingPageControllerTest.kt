b6344a082b0f1090ea70708566abdf4e89a8ab0d
Mikhail Shadrin <shadrin.michael@yandex.ru>
2021-11-30 00:47:35 +0300

    #3 Added simple test template

---
+++ src/test/kotlin/ru/sberschool/filtrobot/controller/LandingPageControllerTest.kt
@@ -0,0 +1,31 @@
+package ru.sberschool.filtrobot.controller
+
+import io.zonky.test.db.AutoConfigureEmbeddedDatabase
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureEmbeddedDatabase(refresh = AutoConfigureEmbeddedDatabase.RefreshMode.BEFORE_EACH_TEST_METHOD)
+internal class LandingPageControllerTest {
+
+    @Autowired
+    private lateinit var mockMvc: MockMvc
+
+    @BeforeEach
+    fun setUp() {
+        //TODO: добавить заполнение тестовыми данными, когда появится логика у лэндинга
+    }
+
+    @Test
+    fun getLandingPage() {
+        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+            .andExpect(MockMvcResultMatchers.status().isOk)
+    }
+}
